2311ec42ed31a3206cf3571fec30e05c2772ac7d
techift <esugift62@gmail.com>
2026-04-24 23:15:15 +0100

    refactor: replaced super.key in splash screen file

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -3,7 +3,7 @@ import 'package:get/get.dart';
 import 'package:social_app/screens/onboarding/onboarding_screen.dart';
 
 class SplashScreen extends StatefulWidget {
-  const SplashScreen({Key? key}) : super(key: key);
+  const SplashScreen({super.key});
 
   @override
   State<SplashScreen> createState() => _SplashScreenState();
