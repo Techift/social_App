1d0665e89ed96f9d91a8e7e5fbe85e74a7c424c7
techift <esugift62@gmail.com>
2026-04-24 23:16:41 +0100

    refactor: replaced super.key in register screen

--- lib/screens/auth/register.dart
+++ lib/screens/auth/register.dart
@@ -6,7 +6,7 @@ import '../../utils/constants.dart';
 import '../../routes/app_routes.dart';
 
 class RegisterScreen extends StatefulWidget {
-  const RegisterScreen({Key? key}) : super(key: key);
+  const RegisterScreen({super.key});
 
   @override
   State<RegisterScreen> createState() => _RegisterScreenState();
